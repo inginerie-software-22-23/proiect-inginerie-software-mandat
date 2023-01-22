f00c5e75360740cdb053095a9c41ffda084b6265
Talida-M <talidutza_marinna@yahoo.com>
2023-01-22 19:21:58 +0200

    Add regex for password and register button is disabled if the input ofr email and password are not valid Add test for requests page (tests passed)

--- Frontend/MANDAT-FE/src/app/pages/mentor-requests/mentor-requests.component.spec.ts
+++ Frontend/MANDAT-FE/src/app/pages/mentor-requests/mentor-requests.component.spec.ts
@@ -1,22 +1,72 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
 
-import { MentorRequestsComponent } from "./mentor-requests.component";
+import { MentorRequestsComponent } from './mentor-requests.component';
+import { RequestModel } from 'src/app/components/interface/request-model';
 
-describe("MentorRequestsComponent", () => {
+describe('MentorRequestsComponent', () => {
   let component: MentorRequestsComponent;
   let fixture: ComponentFixture<MentorRequestsComponent>;
+  let requests: RequestModel[];
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [MentorRequestsComponent],
-    }).compileComponents();
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [MentorRequestsComponent]
+    })
+    .compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(MentorRequestsComponent);
     component = fixture.componentInstance;
+    requests = [{
+      fullName: "student1",
+      email: "student1@yahoo.com",
+      matchDate: '2023-01-21T22:58:50.7697837',
+      status: 'Waiting' 
+    },
+    {
+      fullName: "student2",
+      email: "student2@yahoo.com",
+      matchDate: '2023-01-21T22:58:50.7697837',
+      status: 'Waiting'
+    },
+    {
+      fullName: "student3",
+      email: "student3@yahoo.com",
+      matchDate: '2023-01-21T22:58:50.7697837',
+      status: 'Waiting'
+    }];
+    component.requests = requests;
     fixture.detectChanges();
   });
 
-  it("should create", () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+
+  it('should call acceptStudent when clicked', () => {
+    spyOn(component
+      , 'acceptStudent');
+      let buttons = fixture.debugElement.queryAll(By.css('button'));
+      if (buttons.length > 0) {
+        buttons[0].triggerEventHandler('click',null);
+        expect(component.acceptStudent).toHaveBeenCalledWith('student1@yahoo.com');
+      } else {
+        console.error("There is no button to be clicked");
+      }
+    });
+  
+    it('should call rejectStudent when clicked', () => {
+      spyOn(component, 'rejectStudent');
+      let buttons = fixture.debugElement.queryAll(By.css('button'));
+      if (buttons.length > 1) {
+        buttons[1].triggerEventHandler('click',null);
+        expect(component.rejectStudent).toHaveBeenCalledWith('student2@yahoo.com');
+      } else {
+        console.error("There is no button to be clicked");
+      }
+    });
+  });

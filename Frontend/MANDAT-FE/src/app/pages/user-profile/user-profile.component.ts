a451fff444ec62cd258a5f45d8d0b713166f3b07
Amar Kayed <amar.kayed18@gmail.com>
2023-01-22 22:52:58 +0200

    feat(fe): finish google maps integration

--- Frontend/MANDAT-FE/src/app/pages/user-profile/user-profile.component.ts
+++ Frontend/MANDAT-FE/src/app/pages/user-profile/user-profile.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { UserAccountService } from "src/app/services/user-account.service";
-import { MentorService } from "src/app/services/mentor.service";
 import { UserAccountWithAddress } from "src/app/models/user-account-with-address-model";
 
 declare var google: any;
+const apiKey = "";
 
 @Component({
   selector: "app-user-profile",
@@ -25,51 +25,53 @@ export class UserProfileComponent implements OnInit {
 
   constructor(
     private userAccountService: UserAccountService,
-    private mentorService: MentorService) {
-    // userAccountService.GetUserInfo("ianis@yahoo.com").subscribe(res => {
-    //   console.log(res);
-    // });
-
-    // userAccountService.GetUserInfoWithAddressByEmail("ianis@yahoo.com").subscribe(res => {
-    //   console.log(res);
-    // })
-
-    userAccountService.GetUserInfoWithAddressByEmail("pat@example1.com").subscribe(res => {
-      console.log(res);
-      this.userAccountWithAddress = res;
-      this.userAccountWithAddress.subject = "muschii mei"
-    })
-
-    // mentorService
-    //   .getMentorByEmailStudView("pat@example1.com")
-    //   .subscribe(res => {
-    //     console.log(res);
-    //   });
+  ) {
+    userAccountService
+      .GetUserInfoWithAddressByEmail("pat@example1.com")
+      .subscribe(res => {
+        console.log(res);
+        this.userAccountWithAddress = res;
+      });
   }
 
   ngOnInit() {
-    const address = "1600 Amphitheatre Parkway, Mountain View, CA";
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
-    });
+    // const address = "1600 Amphitheatre Parkway, Mountain View, CA";
+    // const address = `Strada Izvor 2-4, București`
+    const address = `${this.userAccountWithAddress.addressInfo}, ${this.userAccountWithAddress.city}, ${this.userAccountWithAddress.county}`;
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?key=${apiKey}&address=${address}`;
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        const location = data.results[0]?.geometry.location;
+        console.log(this.userAccountWithAddress);
+        console.log(data);
 
-    const geocoder = new google.maps.Geocoder();
-    geocoder.geocode(
-      { address: address },
-      (results: { geometry: { location: any } }[], status: string) => {
-        if (status === "OK") {
-          map.setCenter(results[0].geometry.location);
-          const marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location,
-          });
-        } else {
-          console.log(
-            `Geocode was not successful for the following reason: ${status}`
-          );
-        }
-      }
-    );
+        let latitude = location !== undefined ? location.lat : -34.397;
+        let longitude = location !== undefined ? location.lng : 150.644;
+
+        const map = new google.maps.Map(document.getElementById("map"), {
+          // center: { lat: -34.397, lng: 150.644 },
+          center: { lat: latitude, lng: longitude },
+          zoom: 8,
+        });
+
+        const geocoder = new google.maps.Geocoder();
+        geocoder.geocode(
+          { address: address },
+          (results: { geometry: { location: any } }[], status: string) => {
+            if (status === "OK") {
+              map.setCenter(results[0].geometry.location);
+              const marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location,
+              });
+            } else {
+              console.log(
+                `Geocode was not successful for the following reason: ${status}`
+              );
+            }
+          }
+        );
+      });
   }
 }
